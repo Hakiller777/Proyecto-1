98de24af8f275723e048a9f6d582265d74a7b0e4
Hakiller777 <hugo14eckert14@gmail.com>
2024-09-08 12:51:54 -0400

    Resolucion de conflictos

--- backend/proyecto_scrum/proyecto_scrum/settings.py
+++ backend/proyecto_scrum/proyecto_scrum/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-np+eq8terl*7uk@$(x1ej3)$ma$5muhzzg1pgcw$=qh2v8&28h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
@@ -136,7 +137,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/hakiller/Escritorio/Proyecto-1/staticfiles'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = '/home/hakiller/Escritorio/Proyecto-1/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
