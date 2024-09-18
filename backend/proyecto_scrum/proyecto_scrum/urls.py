152ec4b0cb86275e53aa1a968671b0e8f1f24dcd
Hakiller <hugo14eckert14@gmail.com>
2024-09-18 09:42:13 -0400

    Prueba de rutas y variables de entorno

--- backend/proyecto_scrum/proyecto_scrum/urls.py
+++ backend/proyecto_scrum/proyecto_scrum/urls.py
@@ -4,7 +4,9 @@ from miapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),  # Ruta para la página principal
+    path('', views.bienvenida, name='bienvenida'),  # Ruta para la página principal
     path('miapp/', include('miapp.urls')),  # Incluye las rutas de la aplicación miapp
 ]
+
+
 
